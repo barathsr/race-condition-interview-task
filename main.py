d9d90414c153c1c293d51c83281abd2e3ed5ad7f
S.R.Barathkumar <barathsr67@gmail.com>
2025-09-20 02:43:47 +0530

    [Barath] Fix dotenv file issue

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
+from dotenv import load_dotenv
 from fastapi import FastAPI
 
-from routers import playground, redis, websocket
+from routers import auth, playground, redis, websocket
 
-app = FastAPI()
+load_dotenv()
+
+app = FastAPI(title="Race Condition Interview Task")
+
 
 app.include_router(playground.router)
 app.include_router(websocket.router)
 app.include_router(redis.router)
+app.include_router(auth.router)
 
 
 @app.get("/")
